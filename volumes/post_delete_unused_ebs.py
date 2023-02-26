735a70c908b93014a1a8fc8cbcdd44d15fceb36d
yaacov <yaacov@netapp.com>
2023-02-26 11:42:41 +0200

    Adding changes to file post_delete_unused_ebs.py post_delete_unused_ebs.py

--- volumes/post_delete_unused_ebs.py
+++ volumes/post_delete_unused_ebs.py
@@ -1,170 +1,118 @@
+from botocore import exceptions
+import botocore
 import requests
 import boto3
 from datetime import datetime
+import os
 from decouple import config
-import schedule
-import time
+
+# Slack Webhook URL
+SLACK_URL = config("SHARED_CHANNEL")
 
 # Global Variables
-GLOBAL_REGIONS_DICT: dict = {}
-CURRENT_DATE = datetime.today().strftime("%Y-%m-%d")
-TIME_CURRENT_DATE = datetime.strptime(CURRENT_DATE, "%Y-%m-%d")
+regions_dict = {}
 
 
-# _____________________________________________________________________________
-# Function to call for a Profile account
-# _____________________________________________________________________________
-def validate_selection(
-        selection, possible_values, decrease_num=False, accept_blank=False
-):
-    possible_selections = possible_values
-    if isinstance(possible_values, dict):
-        is_dict = True
-        possible_selections = [i for i in possible_values]
-    try:
-        int(selection)
-        is_number = True
-    except ValueError:
-        is_number = False
-
-    if is_number:
-        check_num = int(selection)
-        if decrease_num:
-            check_num = check_num - 1
-
-        if check_num < len(possible_selections):
-            return possible_selections[check_num]
-        else:
-            return False
-    else:
-        if accept_blank and selection == "":
-            return True
-        if selection in possible_selections:
-            return selection
-        else:
-            return False
-
-
-def get_user_input(options):
+# Function to select profile
+def select_profile(options):
     for i, k in enumerate(options):
-        print("{} ) {}".format(str(i + 1), k))
+        print(f"{i + 1}) {k}")
     selection = input("Enter selection: ")
-    final_selection = validate_selection(selection, options, True)
-    while not final_selection:
-        selection = input("Enter selection: ")
-        final_selection = validate_selection(selection, options, True)
-    return final_selection
+    try:
+        index = int(selection) - 1
+        if index >= 0 and index < len(options):
+            return options[index]
+    except ValueError:
+        pass
+    return None
 
 
-# _______________________________________________________________
-# Function to call Unused EBS
-# _______________________________________________________________
-def describe_unused_ebs(region):
-    regional_client = boto3.resource("ec2", region_name=region)
+# Function to get unused EBS in a region
+def get_unused_ebs(region):
+    client = boto3.resource("ec2", region_name=region)
     volumes = []
-    for volume in regional_client.volumes.filter(
+    for volume in client.volumes.filter(
             Filters=[{"Name": "status", "Values": ["available"]}]
     ):
         if volume.state == "available":
             volume_id = volume.id
-            volumes.append(volume_id)
-            print("ID:", volume.id, "Status: available")
-            volume.delete()
-            print("ID:", volume.id, "Status: deleted")
+            if volume.tags:
+                volume_name = next((tag['Value'] for tag in volume.tags if tag['Key'] == 'Name'), '<No Name Tag>')
+            else:
+                volume_name = '<No Name Tag>'
+            volumes.append({'id': volume_id, 'name': volume_name})
     return volumes
 
 
-# ________________________________________________________________
-# Function to call All Regions
-# ________________________________________________________________
-def available_regions():
-    service: str = "ec2"
-    regions: list = []
-    client = boto3.client(service)
+# Function to get all unused EBS in all regions
+def get_unused_ebs_all_regions():
+    unused_all_regions = {}
+    client = boto3.client("ec2")
     dict_of_regions = client.describe_regions()
     for item in dict_of_regions["Regions"]:
-        regions.append(item["RegionName"])
-        GLOBAL_REGIONS_DICT[item["RegionName"]] = []
-    return regions
+        region = item["RegionName"]
+        unused_ebs = get_unused_ebs(region)
+        if unused_ebs:
+            unused_all_regions[region] = unused_ebs
+    return unused_all_regions
 
 
-# _______________________________________________________________
-# function to call list of Profiles
-# _______________________________________________________________
-def list_of_profiles():
-    all_profiles: list = []
-    for profile in boto3.session.Session().available_profiles:
-        all_profiles.append(profile)
-    return all_profiles
-
-
-# _______________________________________________________________
-# Function to call Unused EBS In All_Regions
-# _______________________________________________________________
-def describe_unused_ebs_in_all_regions():
-    all_unused_all_regions = {}
-    region_list: list = available_regions()
-    for specific_region in region_list:
-        print(f"Checking  region {specific_region}")
-        ebs_list: list = describe_unused_ebs(specific_region)
-        if len(ebs_list) > 0:
-            all_unused_all_regions[specific_region] = ebs_list
-            print("")
+# Function to delete unused EBS in a region
+def delete_unused_ebs(region, volume_id):
+    client = boto3.client("ec2", region_name=region)
+    try:
+        response = client.delete_volume(VolumeId=volume_id)
+        # print(f"Deleted volume {volume_id} in {region}")
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "InvalidVolume.NotFound":
+            pass
         else:
-            print("No available Volumes")
-    return all_unused_all_regions
+            print(f"Error deleting volume {volume_id} in {region} {e}")
 
 
-# __________________________________________________________________
-# Format message
-# __________________________________________________________________
-# accept dictionary that looks like:
-# {"us-east-1": ["vol-123", "vol-456"], "us-west-2": ["vol-789"]}
-# output:
-# """
-# *Unused EBS Volumes*
-# *us-east-1*
-# vol-123
-# vol-456
-# *us-west-2*
-# vol-789
-# """
-def format_message(unused_ebs_dict):
-    message = f"*Unused EBS Volumes in {selected_profile}*\n"
-    for region, volumes in unused_ebs_dict.items():
-        message += f"*{region}*\n"
+# Function to delete all unused EBS in all regions
+def delete_unused_ebs_all_regions(data):
+    for region, volumes in data.items():
         for volume in volumes:
-            message += f"{volume}\n"
-        for region, volumes in unused_ebs_dict.items():
-            message += f"*{region}*\n"
-            for volume in volumes:
-                message += f"{volume} - deleted\n"
+            delete_unused_ebs(region, volume['id'])
+
+
+# Function to format the message
+def format_message(profile, data):
+    message = f"Deleted available EBS in all regions for Account: {profile}:\n"
+    for region, volumes in data.items():
+        message += f"\nRegion: *{region}*\n"
+        message += "```\n"
+        for volume in volumes:
+            message += f"Volume ID: {volume['id']}\n"
+            if volume['name']:
+                message += f"Volume Name: {volume['name']}\n"
+            message += "\n"
+        message += "```\n"
     return message
 
 
-# _________________________________________________________________
-# Call Slack
-# _________________________________________________________________
-def send_message(message):
-    slack_url = config("SLACK_URL")
-    payload_obj1 = '{"text": "%s"}' % message
-    response = requests.post(slack_url, payload_obj1)
+# Function to send slack message
+def send_slack_message(message):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "text": message
+    }
+    response = requests.post(SLACK_URL, headers=headers, json=payload)
     return response.text
 
 
-# _______________________________________________________________
-# Function to call Schedule Time
-# _______________________________________________________________
+# Main function
 def main():
-    message = format_message(describe_unused_ebs_in_all_regions())
-    send_message(message=message)
+    profile = select_profile(boto3.session.Session().available_profiles)
+    boto3.setup_default_session(profile_name=profile)
+    data = get_unused_ebs_all_regions()
+    message = format_message(profile, data)
+    send_slack_message(message)
+    delete_unused_ebs_all_regions(data)
 
 
 if __name__ == "__main__":
-    selected_profile = get_user_input(boto3.session.Session().available_profiles)
-    session = boto3.setup_default_session(profile_name=selected_profile)
-    # schedule.every().day.at("12:15").do(schedule_time_list_unused_ebs)
-main()
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
+    main()
